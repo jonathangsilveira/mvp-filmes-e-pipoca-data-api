9125b0ed19665991b7197076c65ed4c0e8bfc8ff
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-06-19 20:59:13 -0300

    Add schema models

---
+++ app/schema/schemas.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class RateMoviePathModel(BaseModel):
+    """
+    Define contrato para caminho da rota de avaliação de filmes.
+
+    Parâmetros:
+        movie_id: ID do filme do TMDB API.
+    """
+    movie_id: int = 1022789
+
+class RateMovieBodyModel(BaseModel):
+    """
+    Define contrato para avaliar um filme.
+
+    Parâmetros:
+        rate_value: Nota dada ao filme para avaliá-lo. Valores entre 0 e 10.
+    """
+    rate_value: int = 9
+
+class AddToWatchlistBodyModel(BaseModel):
+    """
+    Define contrato para adicionar um filme da lista para assistir.
+
+    Parâmetros:
+        movie_id: ID do filme do TMDB API.
+    """
+    movie_id: int = 1022789
+
+class RemoveFromWatchlistPathModel(BaseModel):
+    """
+    Define contrato para remover um filme da lista para assistir.
+
+    Parâmetros:
+        movie_id: ID do filme do TMDB API.
+    """
+    movie_id: int = 1022789
+
+class WatchlistModel(BaseModel):
+    """
+    Define contrato para exibição de resultados da lista para assistir.
+
+    Parâmetros:
+        movie_ids: Lista de IDs dos filmes registrados na lista para assistir.
+    """
+    movie_ids: list[int]
