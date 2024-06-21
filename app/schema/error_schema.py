03d2c141332b7e85f2ab329ef5fd31f7a985790a
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-06-20 21:45:57 -0300

    Remove file

--- app/schema/error_schema.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class ErroSchema(BaseModel):
-    """
-    Define contrato para exibição de erros da API.
-    """
-    mensagem: str = "Erro ao adicionar filme na watchlist!"
